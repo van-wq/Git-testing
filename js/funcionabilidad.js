1bcb0a0c41b961158f5f5cdb3e8d6ce02cb5d157
Jorge E Lazarte <lazartej71@gmail.com>
2025-10-02 04:25:55 -0300

    Cambio Total en el Proyect

--- js/funcionabilidad.js
+++ js/funcionabilidad.js
@@ -16,3 +16,27 @@ function pedirClave() {
         window.location.href = "index.html";
     }
 }
+//MENU PARA CELULAR
+function toggleMenu() {
+    document.querySelector(".menu").classList.toggle("show");
+}
+//Funcion para verificar la Clave
+function verificarClave() {
+    let clave = document.getElementById("clave").value;
+    if (clave === "libertadores") {
+        document.getElementById("contenidoSecreto").style.display = "grid";
+        document.querySelector(".login").style.display = "none";
+    } else {
+        alert("Clave incorrecta ❌");
+    }
+}
+//Funcion para mostrar si la respuesta del quizz es correcta o incorrecta
+function respuesta(boton, correcto) {
+    if (correcto) {
+        boton.style.background = "green";
+        alert("✅ Correcto!");
+    } else {
+        boton.style.background = "red";
+        alert("❌ Incorrecto, intente otra opción.");
+    }
+}
